7e23498819be2bf072a8bccd960f9f6a513baa65
MattZ45986 <33816924+MattZ45986@users.noreply.github.com>
2018-04-12 20:00:54 -0400

    Update SimonSays.py
    Simon say is a classic game where the players have to do things told by someone, but only if "Simon says."  In this game there are a variety of colored circles laid across the map.  The players are then told to run to the different circles, the top, bottom, left, or right sides of the map, but if they do when Simon doesn't say to then they lose.  It gets faster as you play and it gets increasingly harder to determine what you are and aren't supposed to do.  Overall, a fun mod for Eric Froemling's game: Bombsquad.

--- SimonSays.py
+++ SimonSays.py
@@ -1,5 +1,7 @@
 #SimonSays
+# you had really better do what Simon says...
 import bs
+import random
 
 def bsGetAPIVersion():
     return 4
@@ -22,7 +24,10 @@ class SimonSays(bs.TeamGameActivity):
 
     @classmethod
     def getSettings(cls, sessionType):
-        return [("Epic Mode", {'default': False})]
+        return [("Epic Mode", {'default': False}),
+                ("Enable Jumping", {'default': False}),
+                ("Enable Punching", {'default': False}),
+                ("Enable Picking Up", {'default': False})]
     
     @classmethod
     def getSupportedMaps(cls, sessionType):
@@ -35,7 +40,20 @@ class SimonSays(bs.TeamGameActivity):
     def __init__(self,settings):
         bs.TeamGameActivity.__init__(self,settings)
         if self.settings['Epic Mode']: self._isSlowMotion = True
-        self.roundNum = 1
+        self.info = bs.NodeActor(bs.newNode('text',
+                                                   attrs={'vAttach': 'bottom',
+                                                          'hAlign': 'center',
+                                                          'vrDepth': 0,
+                                                          'color': (0,.2,0),
+                                                          'shadow': 1.0,
+                                                          'flatness': 1.0,
+                                                          'position': (0,0),
+                                                          'scale': 0.8,
+                                                          'text': "Created by MattZ45986 on Github",
+                                                          }))
+        self.roundNum = 0
+        self.simon = False
+        self.time = 5000
         self._r1 = 2
         n1 = bs.newNode('locator',attrs={'shape':'circle','position':(-4,0,-6),
                                          'color':(1,0,0),'opacity':0.5,
@@ -73,29 +91,53 @@ class SimonSays(bs.TeamGameActivity):
         bs.animateArray(n7,'size',1,{0:[0.0],200:[self._r1*2.0]})
         bs.animateArray(n8,'size',1,{0:[0.0],200:[self._r1*2.0]})
         bs.animateArray(n9,'size',1,{0:[0.0],200:[self._r1*2.0]})
+        self.options = ["red", "green", "blue", "yellow", "teal", "purple", "gray", "orange", "white", "top", "bottom", "middle row", "left", "right", "center column", "outside"]
 
     def onTransitionIn(self):
         bs.TeamGameActivity.onTransitionIn(self,music='FlagCatcher')
 
     def onBegin(self):
+        s = self.settings
         bs.TeamGameActivity.onBegin(self)
         for team in self.teams:
             team.gameData['score'] = 0
         for player in self.players:
             player.gameData['score'] = 0
+            player.actor.connectControlsToPlayer(enableBomb=False, enablePunch = s["Enable Punching"], enablePickUp = s["Enable Picking Up"], enableRun = True, enableJump = s["Enable Jumping"])
         self.explainGame()
 
     def explainGame(self):
         bs.screenMessage("Follow the commands...")
         bs.screenMessage("but only when Simon says!")
-        bs.gameTimer(3000, self.callRound)
+        bs.gameTimer(5000, self.callRound)
 
     def callRound(self):
+        self.roundNum += 1
+        self.num = random.randint(0, 15)
+        num = self.num
+        self.simon = random.choice([True, False])
+        if num < 9: line = "Run to the " + self.options[num] + " circle!"
+        elif num < 15: line = "Run to the " + self.options[num] + "!"
+        else: line = "Run outside of the circles!"
+        if self.simon: line = "Simon says " + line[0].lower() + line[1:]
+        bs.screenMessage(line)
+        self.time -= 100
+        bs.gameTimer(self.time, self.checkRound)
+
+    def checkRound(self):
+        for player in self.players:
+            if player.isAlive():
+                safe = True if self.options[self.num] in self.inCircle(player.actor.node.positionCenter) else False
+                if ((self.simon and safe == False) or ((not self.simon) and safe == True)):
+                    player.getTeam().gameData["score"] = self.roundNum
+                    player.actor.handleMessage(bs.DieMessage())
+        self.callRound()
+                        
 
     def inCircle(self, pos):
         circles = []
-        pos[0] = x
-        pos[2] = z
+        x = pos[0]
+        z = pos[2]
         if (x + 4) ** 2 + (z + 6) ** 2 < 4: circles.append("red")
         elif (x) ** 2 + (z + 6) ** 2 < 4: circles.append("green")
         elif (x - 4) ** 2 + (z + 6) ** 2 < 4: circles.append("blue")
@@ -105,17 +147,27 @@ class SimonSays(bs.TeamGameActivity):
         elif (x + 4) ** 2 + (z - 2) ** 2 < 4: circles.append("gray")
         elif (x) ** 2 + (z - 2) ** 2 < 4: circles.append("orange")
         elif (x - 4) ** 2 + (z - 2) ** 2 < 4: circles.append("white")
-        else: circles.append("none")
+        else: circles.append("outside")
         if x < -2: circles.append("left")
         if x > 2: circles.append("right")
-        if x > -2 and x < 2: circles.append("center")
+        if x > -2 and x < 2: circles.append("center column")
+        if z > 0: circles.append("bottom")
+        if z < -4: circles.append("top")
+        if z < 0 and z > -4: circles.append("middle row")
+        return circles
 
     def handleMessage(self, m):
-        if isinstance(m, bs.PlayerSpazDeathMessage): self.endGame()
+        if isinstance(m, bs.PlayerSpazDeathMessage): self.checkEnd()
+        else: bs.TeamGameActivity.handleMessage(self, m)
+
+    def checkEnd(self):
+        i = 0
+        for player in self.players:
+            if player.isAlive(): i += 1
+        if i < 2: self.endGame()
 
     def endGame(self):
         results = bs.TeamGameResults()
         for team in self.teams:
             results.setTeamScore(team, team.gameData['score'])
         self.end(results=results)
-        
